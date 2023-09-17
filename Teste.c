58a810f8ea7843665f121adc51d9c7a8fb5b2c09
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2023-09-17 20:44:24 -0300

    tentando fazer o gerador de código para a maquina

--- Teste.c
+++ Teste.c
@@ -3,10 +3,9 @@
 int main() {
 
     // codificador
-    signed int codVerificador;
-    int numeros,n,hora,dia,mes,senha,mudaValorHora,mudaValorDia,mudaValorMes;
+    int hora, dia, mes, senha, mudaValorHora, mudaValorDia, mudaValorMes;
     int quantidade;
-  
+
     printf("Digite a quantidade de valores a serem inseridos: ");
     scanf("%d", &quantidade);
 
@@ -24,42 +23,18 @@ int main() {
     for (int i = 0; i < quantidade; i++) {
         printf("%d ", valores[i]);
     }
-    printf("\n");
 
 
-  hora = valores[0];
-  dia = valores[1];
-  mes = valores[2];
-  
-  printf("\n %d \n %d \n %d",hora,dia,mes);
+    hora = valores[0];
+    dia = valores[1];
+    mes = valores[2];
 
-  mudaValorHora = hora * 5;
-  mudaValorDia = dia * 6;
-  mudaValorMes = mes * 7;
+
+
+
+    mudaValorHora = hora * 5;
+    mudaValorDia = dia * 6;
+    mudaValorMes = mes * 7;
 
 return 0;
-} 
-
-
-  //printf("%d \n %d \n %d \n",mudaValorHora,mudaValorDia,mudaValorMes);
-  
-  
-
-  // função codigo verificador
-  
-  /*senha = (mudaValorHora*4 + mudaValorDia*3 + mudaValorMes*2)%4;
-
-  if (senha == 0 || senha == 1)
-  {
-    printf("A senha gerada foi: %d%d%d-%d",mudaValorHora,mudaValorDia,mudaValorMes,senha);
-  }
-  else{
-    codVerificador = 4 - senha;
-   printf("A senha gerada foi: %d%d%d-%d",mudaValorHora,mudaValorDia,mudaValorMes,codVerificador);
-  }
-  
-
-  */
-  
-
-  
+}
